345dedddb7284da0f25f1f960cfc80107dd983da
DC <dachang.h@gmail.com>
2019-06-06 15:35:07 +0800

    Add update.js for put method

---
+++ blogs/update.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const AWS =  require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.update = (event, _context, callback) => {
+
+  const timestamp = new Date().getTime()
+  const requestId = event.pathParameters.id
+  const requestBody = JSON.parse(event.body)
+
+  if (typeof requestBody.title !== 'string' || typeof requestBody.content !== 'string') {
+    console.error('Failed to validate');
+    callback(new Error('Attributes Type Error'));
+    return;
+  }
+
+  const params = {
+    TableName: 'blogs',
+    Item: {
+      id: requestId,
+      title: requestBody.title,
+      content: requestBody.content,
+      updatedAt: timestamp
+    }
+  }
+
+  dynamoDb.put(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(new Error(`Something went wrong: ${error}`));
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify(result.Item)
+    }
+
+    callback(null, response)
+  })
+}
